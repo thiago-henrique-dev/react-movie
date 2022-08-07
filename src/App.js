67ea9609feba2c7cbec01ddaf9c41630d35bbe8a
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-06 21:30:10 -0300

    first commit and getMoviesRequest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import MovieList from './components/MoviesList';
 
 const App = () => {
-  const [movies, setMobvies] = useState([])
+  const [movies, setMovies] = useState([])
 
-  return <div>hello!</div>
+  const getMoviesRequest = async () =>{
+    const url = 'http://omdbapi.com/?s=star wars&apikey=1ecc91b'
+    const response = await fetch(url);
+    const responseJson = await response.json()
+    setMovies(responseJson.Search)
+    console.log(responseJson)
+
+  }
+
+  useEffect(() => {
+    getMoviesRequest()
+  }, [])
+
+  return (
+  
+   <div className="container-fluid movie-app">
+    <div className="row">
+       <MovieList movies={movies} />
+    </div>
+   </div>
+
+
+    
+    
+    
+  )
 }
 
 export default App;
---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+
+const MovieList = (props) => {
+
+ 
+  return (
+          <>
+          {props.movies.map(movie => (
+            <div className="d-flex justify-content-start m-3">
+              <img src={movie.Poster} alt="movie"></img>
+            </div>
+          ))}
+          </>
+  
+    
+  )
+    
+}
+
+export default MovieList;
